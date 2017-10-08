a4f95dea4d91aeaa4da297532f53ca75fd3a4194
dac tri <skyphantomhz@gmail.com>
2017-10-08 15:05:47 +0700

    init backend

---
+++ backend/src/main/java/com/cousin/forwhat/ForWhatApplication.java
@@ -0,0 +1,12 @@
+package com.cousin.forwhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForWhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForWhatApplication.class, args);
+	}
+}
