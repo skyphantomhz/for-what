6c4380a1d58efd8d42be8f094e4d4289a1e03c4e
Unknown <trang23396@gmail.com>
2017-10-12 15:24:11 +0700

    luckyLoginRegister
    make LoginPage and RegisterPage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {MatCheckboxModule,MatInputModule, MatButtonModule,MatFormFieldModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    
   ],
   imports: [
-    BrowserModule
+    FormsModule, ReactiveFormsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
