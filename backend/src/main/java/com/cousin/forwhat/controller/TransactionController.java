5b5f010cd510c4bef3158c0123043333e5e12853
dac tri <skyphantomhz@gmail.com>
2017-11-22 17:48:39 +0700

    up

--- backend/src/main/java/com/cousin/forwhat/controller/TransactionController.java
+++ backend/src/main/java/com/cousin/forwhat/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.cousin.forwhat.controller;
 
+import com.cousin.forwhat.DTO.TransactionDTO;
 import com.cousin.forwhat.entity.Account;
 import com.cousin.forwhat.entity.SessionTransaction;
 import com.cousin.forwhat.service.AccountService;
@@ -7,10 +8,8 @@ import com.cousin.forwhat.service.SessionTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -25,5 +24,11 @@ public class TransactionController {
     @GetMapping()
     public ResponseEntity getAllSessionTransaction() {
         return new ResponseEntity<>(sessionTransactionService.findAllSessionTransactions(), HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @PostMapping()
+    public ResponseEntity createNewTransaction(@Validated @RequestBody TransactionDTO item) throws Exception {
+        return new ResponseEntity<>(sessionTransactionService.createNewSessionTransaction(item), HttpStatus.CREATED);
     }
 }
--- backend/src/main/java/com/cousin/forwhat/service/SessionTransactionServiceImpl.java
+++ backend/src/main/java/com/cousin/forwhat/service/SessionTransactionServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cousin.forwhat.service;
 
+import com.cousin.forwhat.DTO.TransactionDTO;
+import com.cousin.forwhat.entity.Account;
 import com.cousin.forwhat.pojo.ReportByMonthPOJO;
 import com.cousin.forwhat.entity.SessionTransaction;
 import com.cousin.forwhat.pojo.ReportInOutComePOJO;
+import com.cousin.forwhat.repository.AccountRepository;
 import com.cousin.forwhat.repository.SessionTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -18,12 +23,10 @@ import java.util.List;
 public class SessionTransactionServiceImpl implements SessionTransactionService  {
 
     @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
     private SessionTransactionRepository sessionTransactionRepository;
-    @Override
-    public SessionTransaction createNewSessionTransaction(SessionTransaction sessionTransaction) {
-        sessionTransactionRepository.save(sessionTransaction);
-        return null;
-    }
 
     @Override
     public List<SessionTransaction> findAllSessionTransactions() {
@@ -47,5 +50,66 @@ public class SessionTransactionServiceImpl implements SessionTransactionService 
         return null;
     }
 
+    //
+    private TransactionDTO transferEntitytoDTO(SessionTransaction item) {
+        TransactionDTO itemDTO = new TransactionDTO();
+        itemDTO.setSessionTransactionId(item.getSessionTransactionId());
+        itemDTO.setAmount((long) item.getAmount());
+        itemDTO.setType(item.getType());
+        itemDTO.setDesciption(item.getDescription());
+        itemDTO.setDatetime(item.getDateTime().toString ());
+        long idAccount = item.getAccount().getUsernameId();
+        itemDTO.setUsernameId(idAccount);
+        String usrname = item.getAccount().getUsername();
+        itemDTO.setUsername(usrname);
+        return itemDTO;
+    }
+
+
+    private SessionTransaction transferDTOtoEntity(TransactionDTO itemDTO) {
+        SessionTransaction item = new SessionTransaction();
+        item.setSessionTransactionId(itemDTO.getSessionTransactionId());
+        item.setAmount((long) itemDTO.getAmount());
+        item.setType(itemDTO.getType());
+        item.setDescription(itemDTO.getDesciption());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = sdf.parse(itemDTO.getDatetime());
+            item.setDateTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        Account account = accountRepository.findOne(itemDTO.getUsernameId());
+        item.setAccount(account);
+
+        return item;
+    }
+
+
+
+    @Override
+    public TransactionDTO createNewSessionTransaction(TransactionDTO transactionDTO) {
+        SessionTransaction item = transferDTOtoEntity(transactionDTO);
+        SessionTransaction done = sessionTransactionRepository.save(item);
+        transactionDTO = transferEntitytoDTO(done);
+        return transactionDTO;
+    }
+
+    @Override
+    public void deleteSessionTransaction(Long id) {
+
+    }
+
+    @Override
+    public TransactionDTO updateSessionTransaction(Long id, TransactionDTO transactionDTO) {
+        return null;
+    }
+
+    @Override
+    public TransactionDTO getTransactionById(Long id) {
+        return null;
+    }
 
 }
